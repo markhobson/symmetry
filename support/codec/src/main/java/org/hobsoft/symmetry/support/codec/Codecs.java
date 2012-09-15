28ede985009d12b6ad9f074dfdfe2a07ac891c74
Mark Hobson <markhobson@gmail.com>
2012-09-15 14:06:24 +0100

    Streamlined symmetry-support-codec to classes actually used by symmetry

--- support/codec/src/main/java/org/hobsoft/symmetry/support/codec/Codecs.java
+++ support/codec/src/main/java/org/hobsoft/symmetry/support/codec/Codecs.java
@@ -13,10 +13,7 @@
  */
 package org.hobsoft.symmetry.support.codec;
 
-import java.nio.charset.Charset;
 import java.util.Map;
-
-import org.hobsoft.symmetry.support.codec.builder.CompoundParticle;
 
 /**
  * Factory class for building codecs.
@@ -33,99 +30,6 @@ public final class Codecs
 	}
 	
 	// public methods ---------------------------------------------------------
-	
-	public static <X, Y> Codec<X, Y> nullCodec()
-	{
-		return NullCodec.get();
-	}
-	
-	public static <X> Codec<X, X> identity()
-	{
-		return IdentityCodec.get();
-	}
-	
-	/**
-	 * Obtains an inverse of the specified codec.
-	 * 
-	 * @param <X>
-	 *            the object type that the specified codec encodes
-	 * @param <Y>
-	 *            the object type that the specified codec decodes
-	 * @param codec
-	 *            the codec to invert
-	 * @return the inverted codec
-	 */
-	public static <X, Y> Codec<Y, X> inverse(Codec<X, Y> codec)
-	{
-		return InverseCodec.get(codec);
-	}
-
-	public static <X, Y, Z> Codec<X, Z> compound(Codec<X, Y> codec1, Codec<Y, Z> codec2)
-	{
-		return CompoundCodec.get(codec1, codec2);
-	}
-	
-	/**
-	 * Initiates creating a compound codec that starts with the specified codec.  This method returns a builder that
-	 * allows subsequent codecs to be added to the chain.
-	 * 
-	 * <p>It is intended to be used as follows:</p>
-	 * 
-	 * <pre>
-	 * Codec&lt;A, C&gt; compoundCodec = CodecBuilder
-	 *     .compound(abCodec)
-	 *     .add(bcCodec)
-	 *     .toCodec();
-	 * </pre>
-	 * 
-	 * @param	codec
-	 * 				the first codec to use in the chain
-	 * @return	a builder that can be used to add further codecs to the chain; use <code>toCodec()</code> to obtain the
-	 * 			resultant compound codec
-	 * @param	<X>
-	 * 				the object type that the first codec can encode
-	 * @param	<Y>
-	 * 				the object type that the first codec can decode
-	 */
-	public static <X, Y> CompoundParticle<X, Y> compound(Codec<X, Y> codec)
-	{
-		return new CompoundParticle<X, Y>(codec);
-	}
-
-	public static <X, Y> Codec<X, Y> notNull(Codec<X, Y> codec)
-	{
-		return NotNullCodec.get(codec);
-	}
-	
-	public static <X, Y> Codec<X, Y> adapt(Encoder<X, Y> encoder)
-	{
-		return adapt(encoder, null);
-	}
-	
-	public static <X, Y> Codec<X, Y> adapt(Decoder<Y, X> decoder)
-	{
-		return adapt(null, decoder);
-	}
-	
-	public static <X, Y> Codec<X, Y> adapt(Encoder<X, Y> encoder, Decoder<Y, X> decoder)
-	{
-		return CodecAdapter.get(encoder, decoder);
-	}
-	
-	public static <X> ByteStreamCodec<X> asByteStream(CharStreamCodec<X> codec)
-	{
-		return new CharByteStreamCodecAdapter<X>(codec);
-	}
-	
-	public static <X> ByteStreamCodec<X> asByteStream(CharStreamCodec<X> codec, String charsetName)
-	{
-		return new CharByteStreamCodecAdapter<X>(codec, charsetName);
-	}
-	
-	public static <X> ByteStreamCodec<X> asByteStream(CharStreamCodec<X> codec, Charset charset)
-	{
-		return new CharByteStreamCodecAdapter<X>(codec, charset);
-	}
 	
 	public static <X, Y> Codec<X, Y> forMap(Map<X, Y> map)
 	{
