a3a5a299461f357c9e3b0244ec8250369197b555
Mark Hobson <markhobson@gmail.com>
2012-09-15 15:54:19 +0100

    Moved jax-rs demo into symmetry subpackage

--- demo/jax-rs/src/main/java/org/hobsoft/symmetry/demo/jaxrs/DemoWindow.java
+++ demo/jax-rs/src/main/java/org/hobsoft/symmetry/demo/jaxrs/DemoWindow.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package jaxrs;
+package org.hobsoft.symmetry.demo.jaxrs;
 
 import org.hobsoft.symmetry.ui.Label;
 import org.hobsoft.symmetry.ui.Window;
--- demo/jax-rs/src/main/java/org/hobsoft/symmetry/demo/jaxrs/HtmlRenderKitProvider.java
+++ demo/jax-rs/src/main/java/org/hobsoft/symmetry/demo/jaxrs/HtmlRenderKitProvider.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package jaxrs;
+package org.hobsoft.symmetry.demo.jaxrs;
 
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
