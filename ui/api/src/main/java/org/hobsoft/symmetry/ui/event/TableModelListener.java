c9101f6261b5a0c7d5efd774f55b3537afd4f552
Mark Hobson <markhobson@gmail.com>
2012-09-15 01:32:05 +0100

    Applied ASL header to all project files

--- ui/api/src/main/java/org/hobsoft/symmetry/ui/event/TableModelListener.java
+++ ui/api/src/main/java/org/hobsoft/symmetry/ui/event/TableModelListener.java
@@ -1,7 +1,15 @@
 /*
- * $HeadURL: https://svn.iizuka.co.uk/components/kozo/tags/0.6.0-beta-1/ui/api/src/main/java/uk/co/iizuka/kozo/ui/event/TableModelListener.java $
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * (c) 2005 IIZUKA Software Technologies Ltd.  All rights reserved.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.hobsoft.symmetry.ui.event;
 
