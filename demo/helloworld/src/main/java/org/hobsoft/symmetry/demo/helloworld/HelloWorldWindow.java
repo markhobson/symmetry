de25770db2dea82aa6df14cedf35527acbd2bb88
Mark Hobson <markhobson@gmail.com>
2012-09-15 15:50:51 +0100

    Moved helloworld demo into symmetry subpackage

--- demo/helloworld/src/main/java/org/hobsoft/symmetry/demo/helloworld/HelloWorldWindow.java
+++ demo/helloworld/src/main/java/org/hobsoft/symmetry/demo/helloworld/HelloWorldWindow.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package helloworld;
+package org.hobsoft.symmetry.demo.helloworld;
 
 import org.hobsoft.symmetry.ui.Label;
 import org.hobsoft.symmetry.ui.Window;
