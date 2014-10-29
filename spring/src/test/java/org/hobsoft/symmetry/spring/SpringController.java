07bb6b664f6ef2c3b31e03aab62862be0dbfd5b2
Mark Hobson <markhobson@gmail.com>
2014-10-29 22:11:08 +0000

    Added initial IT for rendering a window

---
+++ spring/src/test/java/org/hobsoft/symmetry/spring/SpringController.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hobsoft.symmetry.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Spring MVC controller for integration tests.
+ */
+@Controller
+public class SpringController
+{
+	// ----------------------------------------------------------------------------------------------------------------
+	// public methods
+	// ----------------------------------------------------------------------------------------------------------------
+
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	@ResponseBody
+	public String get()
+	{
+		return "<html/>";
+	}
+}
