a9deed5bc21cd4bfc77cb28359b55674300b8feb
Mark Hobson <markhobson@gmail.com>
2014-11-28 21:24:19 +0000

    Added initial taglib IT

---
+++ taglib/src/test/java/org/hobsoft/symmetry/taglib/it/ServerRule.java
@@ -0,0 +1,102 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hobsoft.symmetry.taglib.it;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.rules.ExternalResource;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.ByteStreams;
+
+/**
+ * JUnit rule that manages a Jetty server.
+ */
+public class ServerRule extends ExternalResource
+{
+	// ----------------------------------------------------------------------------------------------------------------
+	// fields
+	// ----------------------------------------------------------------------------------------------------------------
+
+	private Server server;
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// ExternalResource methods
+	// ----------------------------------------------------------------------------------------------------------------
+
+	@Override
+	protected void before()
+	{
+		server = new Server(0);
+	}
+
+	@Override
+	protected void after()
+	{
+		if (server.isRunning())
+		{
+			stopQuietly();
+		}
+	}
+	
+	// ----------------------------------------------------------------------------------------------------------------
+	// public methods
+	// ----------------------------------------------------------------------------------------------------------------
+
+	public Server getServer()
+	{
+		return server;
+	}
+
+	public void startWebApp(URL webAppUrl, String contextPath) throws Exception
+	{
+		server.setHandler(new WebAppContext(webAppUrl.toString(), contextPath));
+		server.start();
+	}
+	
+	public String get(String path) throws IOException
+	{
+		URL serverUrl = server.getURI().toURL();
+		URL url = new URL(serverUrl, path);
+		
+		return toString(url);
+	}
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// private methods
+	// ----------------------------------------------------------------------------------------------------------------
+
+	private static String toString(URL url) throws IOException
+	{
+		InputStream inputStream = url.openStream();
+		byte[] bytes = ByteStreams.toByteArray(inputStream);
+		return new String(bytes, Charsets.UTF_8);
+	}
+	
+	private void stopQuietly()
+	{
+		try
+		{
+			server.stop();
+		}
+		catch (Exception exception)
+		{
+			// ignore
+		}
+	}
+}
---
+++ taglib/src/test/java/org/hobsoft/symmetry/taglib/it/TaglibTest.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.hobsoft.symmetry.taglib.it;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import static com.google.common.io.Resources.getResource;
+
+/**
+ * Integration test for tag library integration.
+ */
+public class TaglibTest
+{
+	// ----------------------------------------------------------------------------------------------------------------
+	// fields
+	// ----------------------------------------------------------------------------------------------------------------
+
+	private ServerRule serverRule = new ServerRule();
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// JUnit methods
+	// ----------------------------------------------------------------------------------------------------------------
+
+	@Rule
+	public ServerRule getServerRule()
+	{
+		return serverRule;
+	}
+
+	// ----------------------------------------------------------------------------------------------------------------
+	// tests
+	// ----------------------------------------------------------------------------------------------------------------
+
+	@Test
+	public void getWindowWithTextReturnsHtml() throws Exception
+	{
+		serverRule.startWebApp(getResource(getClass(), ""), "/");
+		
+		String actual = serverRule.get("/windowWithText.jsp");
+		
+		assertThat(actual, is("<html><body>x</body></html>"));
+	}
+}
